c31278a12746c66da46b88c07fbc37f6e71fdeaf
fritsonagung <agungfritson@gmail.com>
2019-04-05 15:35:29 +0700

    .

--- app/src/main/java/com/example/lat1_akb2_10116076_fritsonagungjuliansayomi/FourthActivity.java
+++ app/src/main/java/com/example/lat1_akb2_10116076_fritsonagungjuliansayomi/FourthActivity.java
@@ -1,8 +1,10 @@
 package com.example.lat1_akb2_10116076_fritsonagungjuliansayomi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FourthActivity extends AppCompatActivity {
@@ -17,5 +19,14 @@ public class FourthActivity extends AppCompatActivity {
 
         TextView tv1 = findViewById(R.id.TV1);
         tv1.setText("Beres! Sekarang "+nama+" udah bisa ngecek penggunaan HP mu tiap hari buat bantu kamu ngatur waktu :)");
+
+        Button button1 = findViewById(R.id.BT1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lat1_akb2_10116076_fritsonagungjuliansayomi/MainActivity.java
+++ app/src/main/java/com/example/lat1_akb2_10116076_fritsonagungjuliansayomi/MainActivity.java
@@ -15,15 +15,16 @@ import android.content.Intent;
 
 /*
     CHANGE LOG
-    1. Kamis 4 April 2019 10.30, Membuat Layout Tampilan Awal
-    2. Kamis 4 April 2019 13.40, Membuat Layout Tampilan Kedua
-    3. Kamis 4 April 2019 14.03, Membuat Layout Tampilan Ketiga
-    4. Kamis 4 April 2019 14.32, Membuat Layout Tampilan Keempat
-    5. Kamis 4 April 2019 15.11, Membuat Fungsi Perpindahan Ke Layout Tampilan Kedua
-    6. Kamis 4 April 2019 15.45, Membuat Fungsi Perpindahan Ke Layout Tampilan Ketiga
-    7. Kamis 4 April 2019 16.01, Membuat Fungsi Perpindahan Ke Layout Keempat
-    8. Kamis 4 April 2019 16.30, Membuat Fungsi Pengambilan Variable Nama dan Umur
-    9. Kamis 4 April 2019 18,00, Mengenerate APK file
+    1.  Kamis 4 April 2019 10.30, Membuat Layout Tampilan Awal
+    2.  Kamis 4 April 2019 13.40, Membuat Layout Tampilan Kedua
+    3.  Kamis 4 April 2019 14.03, Membuat Layout Tampilan Ketiga
+    4.  Kamis 4 April 2019 14.32, Membuat Layout Tampilan Keempat
+    5.  Kamis 4 April 2019 15.11, Membuat Fungsi Perpindahan Ke Layout Tampilan Kedua
+    6.  Kamis 4 April 2019 15.45, Membuat Fungsi Perpindahan Ke Layout Tampilan Ketiga
+    7.  Kamis 4 April 2019 16.01, Membuat Fungsi Perpindahan Ke Layout Keempat
+    8.  Kamis 4 April 2019 16.30, Membuat Fungsi Pengambilan Variable Nama dan Umur
+    9.  Kamis 4 April 2019 18,00, Mengenerate APK file
+    10. Jumat 5 April 2019 15.15, Membuat Fungsi Exit Pada Tombol Oke
 */
 
 public class MainActivity extends AppCompatActivity {
